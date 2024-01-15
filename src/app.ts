3f81448f8fdcf5829ac63a02db3c6357f1677569
fcuestas <77675947+fcuestas@users.noreply.github.com>
2024-01-15 09:04:59 +0100

    S24011501 Añadimos router

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
-import express from "express";
+import express, {urlencoded} from "express";
 
 import { rutas } from "./utils/rutas.js";
+import { adminRouter } from "./routes/adminRoutes.js";
+import { shopRouter } from "./routes/shopRoutes.js";
 
 console.log('------------------------------------------------------------_---');
 console.log("Bienvenido a mi app");
@@ -9,33 +11,27 @@ const port =  3000;
 
 const app = express();
 
+app.use(urlencoded({extended: false})); //Middleware para procesar los campos que me envíen por HTTP body-parser
 app.set('view engine', 'ejs');
 app.set('views',rutas.views); //CAMBIAR
 //Controladores para responder a las peticiones por HTTP
 
-app.get('/saludo', (req,res,next)=>{
-    res.render('prueba',{nombre: 'Ico'});
-})
-
-app.get('/automovil',(req,res,next)=>{
-    console.log("Pasamos por el primer middleware app.get");
-    res.redirect("/coche");
-})
+app.use('/admin', adminRouter);
+app.use('/', shopRouter );
 
 app.use('/coche',(req, res, next) => {
     console.log("Ha llegado una petición");
     next();
 });
-
-app.use('/', (req,res,next)=> {
-    console.log("Middleware del final");
-    res.status(404).send({'error':'mal hecho'});
-})
-
 app.use('/coche', (req,res,next) => {
     console.log("Estamos en el segundo middleware");
     res.send({"message":"ok"});
 });
+
+app.use('/', (req,res,next)=> {
+    console.log("Middleware del final");
+    res.status(404).send({'error':'Ruta no encontrada'});
+})
 
 // FIN 
 app.listen(port);
