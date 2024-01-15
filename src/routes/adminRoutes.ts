3969687e978ac3b1a88ea1080b34a90e36247904
fcuestas <77675947+fcuestas@users.noreply.github.com>
2024-01-15 10:40:57 +0100

    S24011504 array  procducts

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -2,16 +2,35 @@ import { Router } from "express";
 
 export const adminRouter = Router();
 
+export const productos: {
+    title: string,
+    imageUrl: string,
+    description: string,
+    price: number
+}[] = [];
+
 //todas las rutas que llegan aquí empiezan por /admin
 
 adminRouter.get('/add-product',(req,res,next)=>{
     console.log("Devolvemos el formulario para meter productos");
-    res.send('<FORM action="/admin/product" method="POST"> <input type="text" name="producto"> <button type="submit"> Enviar </button> </FORM> ');
+    //res.send('<FORM action="/admin/product" method="POST"> <input type="text" name="producto"> <button type="submit"> Enviar </button> </FORM> ');
+    res.render('add-product',{pageTitle: "Formulario", path: "/admin/add-product"});
 });
 
-adminRouter.post('/product',(req, res, next) => {
-    if(req.body.producto){
-        console.log('Ha llegado el siguiente producto: ',req.body.producto)
+adminRouter.post('/add-product',(req, res, next) => {
+    const title = req.body.title;
+    const imageUrl =  req.body.imageUrl;
+    const description = req.body.description;
+    const price = +req.body.price;
+    if(req.body.title){
+        console.log('Ha llegado el siguiente producto: ',req.body.title);
+        productos.push({
+            title: req.body.title,
+            imageUrl: imageUrl,
+            description: description,
+            price: price
+        });
     }
-    res.redirect('/coche');
+    console.log('pasa')
+    res.redirect('/');  
 });
--- src/routes/shopRoutes.ts
+++ src/routes/shopRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 
+import { productos } from "./adminRoutes.js";
 
 export const shopRouter =  Router();
 
 //Usamos get y por lo tanto exige coincidencia "completa", no capa otras rutas
 shopRouter.get('/', (req,res,next) => {  
-    res.render('shop', {pageTitle:'Tienda', path:'/', prods: []});
+    res.render('shop', {pageTitle:'Tienda', path:'/', prods: productos});
 });
 shopRouter.get('/saludo', (req,res,next)=>{
     res.render('prueba',{nombre: 'Ico'});
